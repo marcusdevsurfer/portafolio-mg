66e16e2d45c484aafd4f63c3375f090c9d996a71
marcusdev99 <marcus.sea13@gmail.com>
2023-11-18 13:51:07 -0600

    feature addition of personal description component to app

--- src/PortafolioApp.jsx
+++ src/PortafolioApp.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Navbar } from './components/Navbar'
+import { PersonalDescription } from './components/PersonalDescription'
 
 export const PortafolioApp = () => {
   return (
     <>
         <Navbar/>
+
+        <PersonalDescription/>
     </>
   )
 }
