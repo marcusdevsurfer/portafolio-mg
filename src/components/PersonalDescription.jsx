f510b17c771e5ec2f5b481a1c2645a82352d622a
marcusdev99 <marcus.sea13@gmail.com>
2023-11-18 13:50:10 -0600

    feature : addition of personal description component

---
+++ src/components/PersonalDescription.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../PersonalDescription.css'
+export const PersonalDescription = () => {
+  return (
+    <div className='personal-description-box'>
+
+        <div className='personal-description-resume'>
+            <h1>Welcome.</h1>
+            <p>I'm Marcus Gonzalez, FullStack Developer with 4 year's of experience.</p>
+        </div>
+
+        <div className='personal-description-projects'>
+            <h3>Projects</h3>
+            <ul>
+                <li>Lorem, ipsum dolor.
+                </li>
+                <li>Lorem, ipsum.</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
