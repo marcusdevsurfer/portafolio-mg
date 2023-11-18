c6ba16f3aece845fc5714d41c8b6da1b9ff7109a
marcusdev99 <marcus.sea13@gmail.com>
2023-11-18 14:23:26 -0600

    refactor: text resume updated

--- src/components/PersonalDescription.jsx
+++ src/components/PersonalDescription.jsx
@@ -5,8 +5,8 @@ export const PersonalDescription = () => {
     <div className='personal-description-box'>
 
         <div className='personal-description-resume'>
-            <h1>Welcome.</h1>
-            <p>I'm Marcus Gonzalez, FullStack Developer with 4 year's of experience.</p>
+              <h1>Welcome.</h1>
+            <p>I'm Marcus Gonzalez, Full Stack Developer with 4 year's of experience.</p>
         </div>
 
         <div className='personal-description-projects'>
