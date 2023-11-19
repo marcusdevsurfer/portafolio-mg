746fdcf88c656dc1ec6125274f67c85709e1ac48
marcusdev99 <marcus.sea13@gmail.com>
2023-11-19 11:11:29 -0600

    feature : addition of Skills component to Personal description component

--- src/components/PersonalDescription.jsx
+++ src/components/PersonalDescription.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../PersonalDescription.css'
+import { Skills } from './Skills'
 export const PersonalDescription = () => {
   return (
     <div className='personal-description-box'>
@@ -7,6 +8,7 @@ export const PersonalDescription = () => {
       <div className='personal-description-resume'>
         <h1>Welcome.</h1>
         <p>I'm Marcus Gonzalez, Full Stack Developer with 4 year's of experience.</p>
+        <Skills/>
       </div>
 
       <div className='personal-description-projects'>
