6bdd23b8afde86a8db177dd617c875df50d74673
marcusdev99 <marcus.sea13@gmail.com>
2023-11-19 14:50:19 -0600

    feature: addition of welcome message component

--- src/components/PersonalDescription.jsx
+++ src/components/PersonalDescription.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import '../PersonalDescription.css'
-import { Skills } from './Skills'
+import { WelcomeMessage } from './WelcomeMessage'
 export const PersonalDescription = () => {
   return (
     <div className='personal-description-box'>
 
-      <div className='personal-description-resume'>
-        <h1>Welcome.</h1>
-        <p>I'm Marcus Gonzalez, Full Stack Developer with 4 year's of experience.</p>
-        <Skills/>
-      </div>
+      <WelcomeMessage/>
 
       <div className='personal-description-projects'>
         <h3>Projects</h3>
---
+++ src/components/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Skills } from './Skills'
+
+export const WelcomeMessage = () => {
+  return (
+    <div className='personal-description-resume'>
+        <h1>Welcome.</h1>
+        <p>I'm Marcus Gonzalez, Full Stack Developer with 4 year's of experience.</p>
+        <Skills/>
+    </div>
+  )
+}
