2afcfacebbf9502ea55ae79b72af992f4867cf02
marcusdev99 <marcus.sea13@gmail.com>
2023-11-19 11:09:57 -0600

    refactor : addition of text and icons in skills components

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { FaJava, FaHtml5, FaReact, FaNpm } from "react-icons/fa";
+import { SiSpringboot, SiMysql } from "react-icons/si";
+import '../Skills.css'
 
 export const Skills = () => {
   return (
-    <div>Skills</div>
+    <div className='skills-box'>
+      <div className='skills-front-end'>
+        <h3 className='skill-title '>Front End</h3>
+        <FaHtml5 className='skill-icon' />
+        <FaReact className='skill-icon' />
+        <FaNpm className='skill-icon' />
+      </div>
+      <div className='skills-back-end'>
+        <h3 className='skill-title '>Back End</h3>
+        <FaJava className='skill-icon' />
+        <SiSpringboot className='skill-icon' />
+        <SiMysql className='skill-icon' />
+
+      </div>
+    </div>
   )
 }
