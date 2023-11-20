2fee0e1a01d23cca329f8778e2915a74f08f15a0
marcusdev99 <marcus.sea13@gmail.com>
2023-11-19 23:38:26 -0600

    refactor : component unification

--- src/components/PersonalDescription.jsx
+++ src/components/PersonalDescription.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import '../PersonalDescription.css'
 import { WelcomeMessage } from './WelcomeMessage'
+import { DeveloperJourney } from './DeveloperJourney'
 export const PersonalDescription = () => {
   return (
     <div className='personal-description-box'>
-
       <WelcomeMessage/>
-
-      <div className='personal-description-projects'>
-        <h3>Projects</h3>
-        <ul>
-          <li>Lorem, ipsum dolor.
-          </li>
-          <li>Lorem, ipsum.</li>
-        </ul>
-      </div>
+      <DeveloperJourney/>
     </div>
   )
 }
