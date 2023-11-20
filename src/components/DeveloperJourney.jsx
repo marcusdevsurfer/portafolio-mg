1661a6736aeac225561acf522e890377a757f2aa
marcusdev99 <marcus.sea13@gmail.com>
2023-11-19 23:37:26 -0600

    feature : creation of developer journey component

---
+++ src/components/DeveloperJourney.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../DeveloperJourney.css'
+
+export const DeveloperJourney = () => {
+  return (
+    <div className='developer-journey-box'>
+        <div className='experience-description'>
+            <h3>HCLTECH</h3>
+            
+            <strong>2021-2023</strong>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, eum id repellendus eaque inventore obcaecati illum unde ex laudantium? Saepe minus ipsum consectetur maiores ut enim nemo dolor, ratione nulla.</p>
+        </div>
+        <div className='experience-description'>
+            <h3>IBM</h3>
+            <strong>2023-Actually</strong>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, eum id repellendus eaque inventore obcaecati illum unde ex laudantium? Saepe minus ipsum consectetur maiores ut enim nemo dolor, ratione nulla.</p>
+        </div>
+    </div>
+  )
+}
