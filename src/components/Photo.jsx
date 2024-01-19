1e7177928459b66ef77edcc03bff81c5f0eecba9
marcusdev99 <marcus.sea13@gmail.com>
2024-01-19 11:36:11 -0600

    addition of photo component and files

---
+++ src/components/Photo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export const Photo = () => {
+  return (
+    <div className='container p-1'>
+        <img src="marcus-photo.png" alt="Profile Photo" className='rounded-circle img-fluid' width={400} />
+    </div>
+  )
+}
