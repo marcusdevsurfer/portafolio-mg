0de780260d0ed573ed33d9b22a4cceddaff36267
marcusdev99 <marcus.sea13@gmail.com>
2024-01-19 11:07:13 -0600

    addition of navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <nav class="navbar navbar-expand-lg bg-black">
+            <div class="container-fluid">
+                <a class="navbar-brand text-white" href="#">Marcus Gonzalez</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="text-white navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </nav>
+    )
+}
