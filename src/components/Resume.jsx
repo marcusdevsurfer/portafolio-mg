214fc4ac21d82f6de632cd6a65218d5f11d7603e
marcusdev99 <marcus.sea13@gmail.com>
2024-01-19 11:36:20 -0600

    addition of resume component

---
+++ src/components/Resume.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Resume = () => {
+  return (
+    <div className='p-1'>
+        <h2>Marcus Gonzalez</h2>
+        <strong>Software Enginner</strong>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laboriosam inventore quos accusamus aliquam provident voluptatum repellat amet vel possimus.</p>
+    </div>
+  )
+}
