8ceacb33dec8b8ce87df74887f721489aa2c7906
marcusdev99 <marcus.sea13@gmail.com>
2024-01-19 11:35:46 -0600

    addition of style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
+const style = {
+    'minHeight': '8vh'
+}
+
 export const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-black">
-            <div class="container-fluid">
-                <a class="navbar-brand text-white" href="#">Marcus Gonzalez</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="text-white navbar-toggler-icon"></span>
-                </button>
-            </div>
-        </nav>
+            <nav style={style} class="navbar navbar-expand-lg bg-black">
+                <div class="container-fluid">
+                    <a class="navbar-brand text-white" href="#">Marcus Gonzalez</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="text-white navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </nav>
     )
 }
