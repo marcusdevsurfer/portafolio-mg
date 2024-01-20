cd26d2fbd080e2f1dbed091e7319975ff6262741
marcusdev99 <marcus.sea13@gmail.com>
2024-01-20 11:39:54 -0600

    change of logo brand

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
     return (
             <nav style={style} class="navbar navbar-expand-lg bg-black">
                 <div class="container-fluid">
-                    <a class="navbar-brand text-white" href="#">Marcus Gonzalez</a>
+                    <a class="navbar-brand border rounded-circle bg-white p-1" href="#">MG</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="text-white navbar-toggler-icon"></span>
                     </button>
