a1b9c499b9cd579cc1895e1cd0740841f7ff2a78
marcusdev99 <marcus.sea13@gmail.com>
2024-01-20 12:07:00 -0600

    photo centered

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Photo = () => {
   return (
-    <div className='container p-1'>
+    <div className='container text-center p-1'>
         <img src="marcus-photo.png" alt="Profile Photo" className='rounded-circle img-fluid' width={400} />
     </div>
   )
