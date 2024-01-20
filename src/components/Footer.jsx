69203fa9279bf3bad70aad9df8f11e7b95cb9695
marcusdev99 <marcus.sea13@gmail.com>
2024-01-20 16:57:56 -0600

    addition of footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='container-fluid bg-black text-white d-flex align-items-center'>
+        <div className='p-2'>
+            <small>Marcus Gonzalez Portafolio</small>
+        </div>
+    </div>
+  )
+}
