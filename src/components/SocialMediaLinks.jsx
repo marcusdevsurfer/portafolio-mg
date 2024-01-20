104b29e18b17f635ad47aaf068394ec48592a478
marcusdev99 <marcus.sea13@gmail.com>
2024-01-20 12:02:25 -0600

    stylesheet addition

--- src/components/SocialMediaLinks.jsx
+++ src/components/SocialMediaLinks.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import { FaLinkedin } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 
-const style = {
-    'color': 'inherit' 
-  }
+import  '/src/style/SocialMediaLinks.css'
 
 export const SocialMediaLinks = () => {
   return (
     <div className='text-center'>
-        <a style={style} target='blank' href="https://www.linkedin.com/in/marcusgonzalez1399/"><FaLinkedin className='m-3 img-fluid' size={30} /></a>
-        <a style={style} target='blank' href="https://github.com/marcusdevsurfer"><FaGithub className='m-3' size={30} /></a>
+        <a className='social-media-link' target='blank' href="https://www.linkedin.com/in/marcusgonzalez1399/"><FaLinkedin className='m-3 img-fluid' size={32} /></a>
+        <a className='social-media-link' target='blank' href="https://github.com/marcusdevsurfer"><FaGithub className='m-3' size={32} /></a>
     </div>
   )
 }
