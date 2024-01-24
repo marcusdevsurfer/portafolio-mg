2b94ee6af40b9f25864edfa7062a42036247780e
marcusdev99 <marcus.sea13@gmail.com>
2024-01-24 13:02:55 -0600

    addition of contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Contact = () => {
+  return (
+    <div className='d-flex flex-column align-items-center container bg-secondary'>
+        <div>
+            Contac Me!
+        </div>
+        <div>
+            <form action="">
+                <input placeholder='Value' className='form-control' type="text" />
+            </form>
+        </div>
+    </div>
+  )
+}
