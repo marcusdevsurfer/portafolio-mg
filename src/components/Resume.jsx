592829f23283878e55c5b1b2a3d44c7775c85469
marcusdev99 <marcus.sea13@gmail.com>
2024-01-24 13:11:52 -0600

    text modification

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,7 +6,7 @@ export const Resume = () => {
     <div className='p-1'>
         <h2>Marcus Gonzalez</h2>
         <strong>Software Enginner</strong>
-        <p>I’m Software Engineer with 4 years of experience developing web applications. Contribuiting in the Back End side creating Web Applications based on Java programming lenguaje in union with Spring Boot to develop REST services and applications based on Micro-services architecture.</p>
+        <p>I’m Software Engineer with 4 years of experience developing web applications. Contribuiting in the Back End side creating Web Applications based on Java programming language in union with Spring Boot to develop REST services and applications based on Micro-services architecture.</p>
         <SocialMediaLinks/>
     </div>
   )
