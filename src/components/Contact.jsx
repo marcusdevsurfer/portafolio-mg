9af53caff0a6ad4809ccfe43eafbda45d21d39b5
marcusdev99 <marcus.sea13@gmail.com>
2024-01-25 11:32:09 -0600

    addition of form clean up

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Contact = () => {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
 
   const handleSubmitFormClick = (e) => {
     e.preventDefault()
     alert("I'm working on this function. It will be available soon be patient.")
+    setName('')
+    setEmail('')
+    setMessage('')
 
   }
 
@@ -16,24 +23,24 @@ export const Contact = () => {
       </div>
 
       <div>
-        <form>
+        <form onSubmit={handleSubmitFormClick}>
 
           <div className='mb-2'>
             <label htmlFor="name" className='form-label'>Name</label>
-            <input id='name' name='name' className='form-control' type="text" />
+            <input value={name} id='name' name='name' className='form-control' type="text" onChange={e => setName(e.target.value)} />
           </div>
 
           <div className='mb-2'>
             <label htmlFor="mail" className='form-label'>Email</label>
-            <input id='mail' name='mail' className='form-control' type="text" />
+            <input value={email} id='mail' name='mail' className='form-control' type="text" onChange={e => setEmail(e.target.value)} />
           </div>
 
           <div className='mb-2'>
-            <textarea className='form-control' placeholder='Message' name="" id="" cols="30" rows="10"></textarea>
+            <textarea value={message} className='form-control' placeholder='Message' name="message" id="message" cols="30" rows="10" onChange={e => setMessage(e.target.value)}></textarea>
           </div>
 
           <div className='text-center my-3'>
-            <input className='btn bg-white text-black' type="submit" value={'Enviar'} onClick={handleSubmitFormClick} />
+            <input className='btn bg-white text-black' type="submit" value={'Enviar'} />
           </div>
         </form>
       </div>
